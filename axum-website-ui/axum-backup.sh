861089ea432aa2d1f870c81464a92a660b7d8b97
Anton Prins <a.prins@d-r.nl>
2010-05-03 11:10:51 +0200

    axum-website-ui: Upgraded PKGBUILD to version 4

---
+++ axum-website-ui/axum-backup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo -e "HTTP/1.0 200 OK\r"
+echo -e "Content-Type: application/octet-stream\r"
+echo -e "Content-Disposition: attachment; filename=\"axum_surface_backup_$( date '+%F_%H-%M-%S').raw\""
+echo -e "Content-Length: $( fdisk /dev/sda -l | grep 'Disk /dev/sda' | awk '{ print $5 }')\r"
+echo -e "Connection: close\r"
+echo -e "\r"
+dd if=/dev/sda 2> /dev/null
+
+
